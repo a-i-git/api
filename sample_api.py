d021b066cb3761e5c2fb8889e77454a4bc68ed89
a-i-git <aiforgit2012@gmail.com>
2025-03-25 19:56:03 +0530

    API PUSH

---
+++ sample_api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/',methods=["GET"])
+def hello():
+    return "Hello world"
+@app.route('/add', methods=['POST'])
+def add1():
+    print("Received Headers:", request.headers)
+    print("Received Body:", request.data)
+    if request.headers.get('Content-Type') != 'application/json':
+        return jsonify({'error': 'Content-Type must be application/json'}), 400
+
+    try:
+        data = request.get_json()
+        if data is None:
+            return jsonify({'error': 'Invalid JSON data'}), 400
+
+        return jsonify({'message': 'Success', 'received_data': data}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
